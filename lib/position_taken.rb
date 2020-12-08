3f6e5f24879d35b2c209e059e03f674c314611bd
Taylor Treece <tayloratreece@gmail.com>
2020-12-08 19:11:31 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
-# code your #position_taken? method here!
+require "pry"
+
+def position_taken?(board, index)
+  binding.pry
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+ end
+end
